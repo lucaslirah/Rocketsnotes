4000865489a06703131b662e2db1c738f1e38f9b
LucasLiraSilva <lucasliraone@gmail.com>
2023-09-03 18:09:14 -0300

    Hello, world test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Details } from './Details'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>,
 )
