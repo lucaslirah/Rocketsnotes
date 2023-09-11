187dfd44d1dc491b858f9f157ee085d08ac3a566
lucaslira <lucasliraone@gmail.com>
2023-09-11 20:04:41 -0300

    creating component Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,13 @@
 import { Container } from "./styles.js"
-import { Button } from "../../components/Button/index.jsx"
+import { Header } from "../../components/Header"
+import { Button } from "../../components/Button"
+
 export function Details() {
   
   return (
     <Container>
-      <h1>Hello, world!</h1>
-      <span>Lucas Lira</span>
-
-      <Button title="Login" loading/>
-      <Button title="Return"/>
-      <Button title="Home"/>
+      <Header/>
+      <Button title="Voltar"/>
     </Container>
   )
 }
