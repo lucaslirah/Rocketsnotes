ae30c4cacbe06c6705a9a1c2788bb75b61b56dab
LucasLiraSilva <lucasliraone@gmail.com>
2023-09-12 21:52:03 -0300

    component ButtonText created

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,22 +3,29 @@ import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
 import { Button } from "../../components/Button"
 import { Tag } from "../../components/Tag"
+import { ButtonText } from "../../components/ButtonText"
+
 
 export function Details() {
   
   return (
     <Container>
       <Header/>
+
+      <ButtonText title="Delete note"/>
+
       <Section title="Useful Links">
         <Links>
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
         </Links>
       </Section>
+
       <Section title="Markers">
         <Tag title="nodejs"/>
         <Tag title="express"/>
       </Section>
+
       <Button title="Return"/>
     </Container>
   )
