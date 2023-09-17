fc49115e57b6a03ef2959d2430750869b4ba2147
LucasLiraSilva <lucasliraone@gmail.com>
2023-09-17 18:54:12 -0300

    adding the avatar element

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { Container, Form } from "./styles"
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi"
+import { Container, Form, Avatar } from "./styles"
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
 import { Button } from "../../components/Button"
 import { Input } from "../../components/Input"
 
@@ -13,6 +13,15 @@ export function Profile(){
             </header>
 
             <Form>
+                <Avatar>
+                    <img src="https://github.com/lucaslirah.png" alt="user's image"/>
+
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+                        <input id="avatar" type="file"/>
+                    </label>
+                </Avatar>
+                
                 <Input
                 type="text"
                 placeholder="Name"
