e71c740bf999ab83eafb3ee35dbc64b84ce5c894
LucasLiraSilva <lucasliraone@gmail.com>
2023-09-17 15:23:24 -0300

    Sign In component at pages created

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,34 @@
-import { Container } from './styles'
+import { Container, Form } from './styles'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { FiMail, FiLock } from 'react-icons/fi'
 
 export function SignIn(){
     return(
         <Container>
-            
+            <Form>
+                <h1>Rocketnotes</h1>
+                <p>Application to save and manage your useful links.</p>
+
+                <h2>Make your login</h2>
+
+                <Input 
+                type="text"
+                placeholder='E-mail'
+                icon={FiMail} 
+                />
+                <Input 
+                type="password"
+                placeholder='Password'
+                icon={FiLock} 
+                />
+
+                <Button title="Login"/>
+
+                <a href="#">
+                    Create an account
+                </a>
+            </Form>
         </Container>
     )
 }
