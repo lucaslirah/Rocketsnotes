71d22d9de194208548542b15126c9d9f739d095f
LucasLiraSilva <lucasliraone@gmail.com>
2023-11-12 21:11:09 -0300

    Carregando dados do usuário - componente Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import { useAuth } from '../../hooks/auth'
+
 import { Container, Form, Avatar } from "./styles"
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
 import { Button } from "../../components/Button"
@@ -5,6 +8,14 @@ import { Input } from "../../components/Input"
 import { Link } from 'react-router-dom'
 
 export function Profile(){
+
+    const { user } = useAuth()
+
+    const [name, setName] = useState(user.name)
+    const [email, setEmail] = useState(user.email)
+    const [passwordOld, setPasswordOld] = useState()
+    const [passwordNew, setPasswordNew] = useState()
+
     return(
         <Container>
             <header>
@@ -27,21 +38,27 @@ export function Profile(){
                 type="text"
                 placeholder="Name"
                 icon={FiUser}
+                onChange={e => setName(e.targe.value)}
+                value={name}
                 />
                 <Input
                 type="text"
                 placeholder="E-mail"
                 icon={FiMail}
-                />
-                <Input
-                type="password"
-                placeholder="Old Password"
-                icon={FiLock}
+                onChange={e => setEmail(e.targe.value)}
+                value={email}
                 />
                 <Input
                 type="password"
                 placeholder="Current Password"
                 icon={FiLock}
+                onChange={e => setPasswordOld(e.targe.value)}
+                />
+                <Input
+                type="password"
+                placeholder="New Password"
+                icon={FiLock}
+                onChange={e => setPasswordNew(e.targe.value)}
                 />
 
                 <Button title="Save"/>
